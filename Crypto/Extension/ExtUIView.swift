a8e162250ac3c134513e1e87aeabd9dbb2f4a8a9
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-09-05 14:46:03 +0300

    added authVC and button segue for them

---
+++ Crypto/Extension/ExtUIView.swift
@@ -0,0 +1,9 @@
+//
+//  ExtUIView.swift
+//  Crypto
+//
+//  Created by Kaiserdem on 05.09.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import Foundation
---
+++ Crypto/Extension/ExtUIViewShadow.swift
@@ -0,0 +1,9 @@
+//
+//  ExtUIViewShadow.swift
+//  Crypto
+//
+//  Created by Kaiserdem on 05.09.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import Foundation
